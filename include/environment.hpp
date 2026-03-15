d7f3a5e711356d3bc65cd09a26ea9c473684e620
Acerx-AMJ <ajm.acerx@gmail.com>
2026-03-15 19:53:15 +0200

    Implemented environment.

---
+++ include/environment.hpp
@@ -0,0 +1,25 @@
+#ifndef SSL_ENVIRONMENT_HPP
+#define SSL_ENVIRONMENT_HPP
+
+#include <string>
+#include <unordered_map>
+
+struct ValueId {};
+
+struct Environment {
+   Environment(Environment *parent);
+   Environment();
+
+   void declare(const std::string &identifier, ValueId value, size_t line);
+   void assign(const std::string &identifier, ValueId value, size_t line);
+   bool exists(const std::string &identifier);
+   ValueId get(const std::string &identifier, size_t line);
+   Environment &resolve(const std::string &identifier, size_t line);
+
+   // Members
+
+   Environment *parent;
+   std::unordered_map<std::string, ValueId> variables;
+};
+
+#endif
---
+++ source/environment.cpp
@@ -0,0 +1,44 @@
+#include "environment.hpp"
+#include "error.hpp"
+
+Environment::Environment(Environment *parent)
+   : parent(parent) {}
+
+Environment::Environment(): parent(nullptr) {
+
+}
+
+void Environment::declare(const std::string &identifier, ValueId value, size_t line) {
+   if (variables.find(identifier) != variables.end()) {
+      raiseError(line, "Cannot shadow variable in the same scope.");
+   }
+   variables[identifier] = value;
+}
+
+void Environment::assign(const std::string &identifier, ValueId value, size_t line) {
+   Environment &env = resolve(identifier, line);
+   env.variables[identifier] = value;
+}
+
+bool Environment::exists(const std::string &identifier) {
+   if (variables.find(identifier) != variables.end()) {
+      return true;
+   }
+   return parent && parent->exists(identifier);
+}
+
+ValueId Environment::get(const std::string &identifier, size_t line) {
+   Environment &env = resolve(identifier, line);
+   return env.variables[identifier];
+}
+
+Environment &Environment::resolve(const std::string &identifier, size_t line) {
+   if (variables.find(identifier) != variables.end()) {
+      return *this;
+   }
+
+   if (!parent) {
+      raiseError(line, "Variable '%s' does not exist in the given scope.", identifier.c_str());
+   }
+   return parent->resolve(identifier, line);
+}
